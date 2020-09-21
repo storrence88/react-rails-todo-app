3636c945e42264ce6eabde846d812e9a3668ab66
Steven Torrence <storrence88@gmail.com>
2020-09-21 14:39:45 -0500

    TodoItems component

---
+++ app/javascript/packs/components/TodoItems.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TodoItems = () => {
+  return (
+    <React.Fragment>
+      <div className='table-responsive'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th scope='col'>Status</th>
+              <th scope='col'>Item</th>
+              <th scope='col' className='text-right'>
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>{this.props.children}</tbody>
+        </table>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default TodoItems;
